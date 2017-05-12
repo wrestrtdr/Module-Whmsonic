a4bb6725be83baa1a1dfe6f860ce958c7ecda256
Abdy Franco <yosoy@abdyfran.co>
2017-05-12 16:44:13 +0000

    Apply fixes from StyleCI

--- api/whmcsonic_api.php
+++ api/whmcsonic_api.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Whmsonic API
+ * Whmsonic API.
  *
  * @package blesta
  * @subpackage blesta.components.modules.whmsonic.whmsonicapi
  * @copyright Copyright (c) 2010, Phillips Data, Inc.
  * @license http://www.blesta.com/license/ The Blesta License Agreement
- * @link http://www.blesta.com/ Blesta
+ * @see http://www.blesta.com/ Blesta
  */
 class WhmsonicApi
 {
@@ -16,7 +16,10 @@ class WhmsonicApi
     private $use_ssl;
 
     /**
-     * Initializes the class
+     * Initializes the class.
+     * @param mixed $password
+     * @param mixed $ip_address
+     * @param mixed $use_ssl
      */
     public function __construct($password, $ip_address, $use_ssl)
     {
@@ -26,7 +29,7 @@ class WhmsonicApi
     }
 
     /**
-     * Return a string containing the last error for the current session
+     * Return a string containing the last error for the current session.
      * @param string $command the Vesta API command to call
      * @param array $params the parameters to include in the API request
      * @return mixed string|Array the curl error message or an array representing the API response
@@ -34,26 +37,25 @@ class WhmsonicApi
     private function apiRequest($command, array $params)
     {
         $curl = curl_init();
-        $params["cmd"] = $command;
-        $params["ip"] = $this->ip_address;
+        $params['cmd'] = $command;
+        $params['ip'] = $this->ip_address;
 
         $params = http_build_query($params);
 
-        $url = "";
-        $port = "2086";
+        $url = '';
+        $port = '2086';
 
-        if ($this->use_ssl == "true") {
-            $url .= "https://";
-            $port = "2087";
-        } else if ($this->use_ssl == "false") {
-            $url .= "http://";
-            $port = "2086";
+        if ($this->use_ssl == 'true') {
+            $url .= 'https://';
+            $port = '2087';
+        } elseif ($this->use_ssl == 'false') {
+            $url .= 'http://';
+            $port = '2086';
         }
-        $url .= $this->ip_address . ":" . $port . "/whmsonic/modules/api.php";
-
+        $url .= $this->ip_address . ':' . $port . '/whmsonic/modules/api.php';
 
         curl_setopt($curl, CURLAUTH_BASIC, CURLAUTH_ANY);
-        curl_setopt($curl, CURLOPT_USERPWD, "root:" . $this->password);
+        curl_setopt($curl, CURLOPT_USERPWD, 'root:' . $this->password);
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
@@ -72,7 +74,7 @@ class WhmsonicApi
     }
 
     /**
-     * Creates a user account
+     * Creates a user account.
      * @param array $params an array of parameters
      * @param string $type specifies whether the account is 'internal' or 'External'
      * @return array an array representing the status of the operation
@@ -80,51 +82,51 @@ class WhmsonicApi
     public function createRadio(array $params, $type = 'internal')
     {
         $status = false;
-        $parameters = array();
-        $parameters["ctype"] = $type;
-        $parameters["esend"] = 'yes'; // always send confirmation email to client
+        $parameters = [];
+        $parameters['ctype'] = $type;
+        $parameters['esend'] = 'yes'; // always send confirmation email to client
 
-        if (isset($params["username"])) {
-            $parameters["rad_username"] = $params["username"];
+        if (isset($params['username'])) {
+            $parameters['rad_username'] = $params['username'];
         }
-        if (isset($params["pass"])) {
-            $parameters["pass"] = $params["pass"];
+        if (isset($params['pass'])) {
+            $parameters['pass'] = $params['pass'];
         }
-        if (isset($params["bitrate"])) {
-            $parameters["bitrate"] = $params["bitrate"];
+        if (isset($params['bitrate'])) {
+            $parameters['bitrate'] = $params['bitrate'];
         }
-        if (isset($params["hspace"])) {
-            $parameters["hspace"] = $params["hspace"];
+        if (isset($params['hspace'])) {
+            $parameters['hspace'] = $params['hspace'];
         }
-        if (isset($params["autodj"])) {
-            $parameters["autodj"] = $params["autodj"];
+        if (isset($params['autodj'])) {
+            $parameters['autodj'] = $params['autodj'];
         }
-        if (isset($params["bandwidth"])) {
-            $parameters["bw"] = $params["bandwidth"];
+        if (isset($params['bandwidth'])) {
+            $parameters['bw'] = $params['bandwidth'];
         }
-        if (isset($params["listeners"])) {
-            $parameters["limit"] = $params["listeners"];
+        if (isset($params['listeners'])) {
+            $parameters['limit'] = $params['listeners'];
         }
-        if (isset($params["client_email"])) {
-            $parameters["cemail"] = $params["client_email"];
+        if (isset($params['client_email'])) {
+            $parameters['cemail'] = $params['client_email'];
         }
-        if (isset($params["client_name"])) {
-            $parameters["cname"] = $params["client_name"];
+        if (isset($params['client_name'])) {
+            $parameters['cname'] = $params['client_name'];
         }
 
-        $response = $this->apiRequest("create", $parameters);
-        if ($response == "Complete") {
+        $response = $this->apiRequest('create', $parameters);
+        if ($response == 'Complete') {
             $status = true;
         }
 
-        return array(
+        return [
             'status' => $status,
             'response' => $response,
-        );
+        ];
     }
 
     /**
-     * Suspends a user account
+     * Suspends a user account.
      * @param string $username the account's username to suspend
      * @return array an array representing the status of the operation
      */
@@ -132,21 +134,21 @@ class WhmsonicApi
     {
         $status = false;
 
-        $response = $this->apiRequest("suspend", array(
+        $response = $this->apiRequest('suspend', [
             'rad_username' => $username,
-        ));
-        if ($response == "Complete") {
+        ]);
+        if ($response == 'Complete') {
             $status = true;
         }
 
-        return array(
+        return [
             'status' => $status,
             'response' => $response,
-        );
+        ];
     }
 
     /**
-     * Un-suspends a user account
+     * Un-suspends a user account.
      * @param string $username the account's username to un-suspend
      * @return array an array representing the status of the operation
      */
@@ -154,21 +156,21 @@ class WhmsonicApi
     {
         $status = false;
 
-        $response = $this->apiRequest("unsuspend", array(
+        $response = $this->apiRequest('unsuspend', [
             'rad_username' => $username,
-        ));
-        if ($response == "Complete") {
+        ]);
+        if ($response == 'Complete') {
             $status = true;
         }
 
-        return array(
+        return [
             'status' => $status,
             'response' => $response,
-        );
+        ];
     }
 
     /**
-     * terminates a user account
+     * terminates a user account.
      * @param string $username the account's username to terminate
      * @return array an array representing the status of the operation
      */
@@ -176,18 +178,16 @@ class WhmsonicApi
     {
         $status = false;
 
-        $response = $this->apiRequest("terminate", array(
+        $response = $this->apiRequest('terminate', [
             'rad_username' => $username,
-        ));
-        if ($response == "Complete") {
+        ]);
+        if ($response == 'Complete') {
             $status = true;
         }
 
-        return array(
+        return [
             'status' => $status,
             'response' => $response,
-        );
+        ];
     }
-
-
 }
